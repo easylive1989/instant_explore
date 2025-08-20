b6f2019d0d1cc36987d6d667d9b9f54b42d37863
paulwu <easylive1989@gmail.com>
2025-08-20 22:44:24 +0800

    refactor: format simple_widget_test

--- frontend/test/widget/simple_widget_test.dart
+++ frontend/test/widget/simple_widget_test.dart
@@ -5,11 +5,7 @@ import '../../lib/screens/login_screen.dart';
 void main() {
   group('Login Screen Test', () {
     testWidgets('should display login screen', (WidgetTester tester) async {
-      await tester.pumpWidget(
-        const MaterialApp(
-          home: LoginScreen(),
-        ),
-      );
+      await tester.pumpWidget(const MaterialApp(home: LoginScreen()));
 
       expect(find.text('Instant Explore'), findsOneWidget);
       expect(find.text('隨性探點'), findsOneWidget);
