15ba1a2dc31e4276c89d183e808e116686e5a405
paulwu <easylive1989@gmail.com>
2025-12-12 23:07:08 +0800

    refactor: replace GetCurrentLocation and GetNearbyPlaces use cases with SearchNearbyPlacesUseCase

--- frontend/lib/features/places/providers.dart
+++ frontend/lib/features/places/providers.dart
@@ -1,9 +1,8 @@
 import 'package:context_app/core/config/api_config.dart';
+import 'package:context_app/features/places/application/search_nearby_places_use_case.dart';
 import 'package:context_app/features/places/infrastructure/geolocator_service.dart';
 import 'package:context_app/features/places/infrastructure/services/places_api_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:context_app/features/places/application/get_current_location_use_case.dart';
-import 'package:context_app/features/places/application/get_nearby_places_use_case.dart';
 import 'package:context_app/features/places/domain/repositories/places_repository.dart';
 import 'package:context_app/features/places/domain/services/location_service.dart';
 import 'package:context_app/features/places/infrastructure/repositories/places_repository_impl.dart';
@@ -25,26 +24,16 @@ final placesRepositoryProvider = Provider<PlacesRepository>((ref) {
 });
 
 // Use Case Providers
-final getCurrentLocationUseCaseProvider = Provider<GetCurrentLocationUseCase>((
+final searchNearbyPlacesUseCaseProvider = Provider<SearchNearbyPlacesUseCase>((
   ref,
 ) {
   final locationService = ref.watch(locationServiceProvider);
-  return GetCurrentLocationUseCase(locationService);
-});
-
-final getNearbyPlacesUseCaseProvider = Provider<GetNearbyPlacesUseCase>((ref) {
   final repository = ref.watch(placesRepositoryProvider);
-  return GetNearbyPlacesUseCase(repository);
+  return SearchNearbyPlacesUseCase(locationService, repository);
 });
 
 // UI-facing Providers
-final currentLocationProvider = FutureProvider<PlaceLocation>((ref) async {
-  final useCase = ref.watch(getCurrentLocationUseCaseProvider);
-  return useCase.execute();
-});
-
 final nearbyPlacesProvider = FutureProvider<List<Place>>((ref) async {
-  final location = await ref.watch(currentLocationProvider.future);
-  final useCase = ref.watch(getNearbyPlacesUseCaseProvider);
-  return useCase.execute(location);
+  final useCase = ref.watch(searchNearbyPlacesUseCaseProvider);
+  return useCase.execute();
 });
---
+++ frontend/test/features/places/application/search_nearby_places_use_case_test.dart
@@ -0,0 +1,45 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:context_app/features/places/application/search_nearby_places_use_case.dart';
+import 'package:context_app/features/places/domain/repositories/places_repository.dart';
+import 'package:context_app/features/places/domain/services/location_service.dart';
+import 'package:context_app/features/places/models/place.dart';
+
+class MockLocationService extends Mock implements LocationService {}
+class MockPlacesRepository extends Mock implements PlacesRepository {}
+class FakePlaceLocation extends Fake implements PlaceLocation {}
+
+void main() {
+  late SearchNearbyPlacesUseCase useCase;
+  late MockLocationService mockLocationService;
+  late MockPlacesRepository mockPlacesRepository;
+
+  setUpAll(() {
+    registerFallbackValue(FakePlaceLocation());
+  });
+
+  setUp(() {
+    mockLocationService = MockLocationService();
+    mockPlacesRepository = MockPlacesRepository();
+    useCase = SearchNearbyPlacesUseCase(mockLocationService, mockPlacesRepository);
+  });
+
+
+  test('should get current location and return nearby places from the repository', () async {
+    // arrange
+    final testLocation = PlaceLocation(latitude: 12.34, longitude: 56.78);
+    final testPlaces = [
+      Place(id: '1', name: 'Test Place 1', formattedAddress: '', location: testLocation, types: [], photos: []),
+      Place(id: '2', name: 'Test Place 2', formattedAddress: '', location: testLocation, types: [], photos: []),
+    ];
+
+    when(() => mockLocationService.getCurrentLocation()).thenAnswer((_) async => testLocation);
+    when(() => mockPlacesRepository.getNearbyPlaces(testLocation)).thenAnswer((_) async => testPlaces);
+
+    // act
+    final result = await useCase.execute();
+
+    // assert
+    expect(result, testPlaces);
+  });
+}
